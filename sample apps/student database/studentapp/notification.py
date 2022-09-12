ce1e081757814288ba62f35fc0dc57428b10f01a
mjOmandam07 <mjomandam@gmail.com>
2022-09-12 12:57:22 +0800

    Attempting to extract the notification message and reflect it in the UI

--- sample apps/student database/studentapp/notification.py
+++ sample apps/student database/studentapp/notification.py
@@ -5,11 +5,13 @@ from pubnub.pubnub import PubNub
 
 import studentapp.models as models
 
-pnconfig = PNConfiguration()
 
+pnconfig = PNConfiguration()
 pnconfig.subscribe_key = 'sub-c-929f34e0-ac3c-4ac1-9203-662b20f90279'
 pnconfig.publish_key = 'pub-c-b0c69ce9-13c4-4ee1-8995-c829d3f410c7'
-pnconfig.user_id = "my_custom_user_id"
+pnconfig.user_id = "my_user_id"
+pnconfig.ssl = True
+
 pubnub = PubNub(pnconfig)
 
 
@@ -21,21 +23,20 @@ def my_publish_callback(envelope, status):
 
 
 class MySubscribeCallback(SubscribeCallback):
-    def presence(self, pubnub, presence):
-        pass
-    def status(self, pubnub, status):
-        pass
+
     def message(self, pubnub, message):
-        print(message.message)
+        print(message.message['text'])
+        return message.message
+
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels('my_channel').execute()
 
 
 class notifications(object):
     def __init__(self, id=None):
         self.id = id
 
-
-    pubnub.add_listener(MySubscribeCallback())
-    pubnub.subscribe().channels("my_channel").execute()
+    
 
     def verify_id(self, id):
         student = models.students(id_number=id)
@@ -45,6 +46,18 @@ class notifications(object):
     def sent_event(self):
         id = self.verify_id(self.id)
         event = f'New Student Added - {id}'
-        pubnub.publish().channel("my_channel").message({str(event)}).pn_async(my_publish_callback)
+        
+        # info = pubnub.publish({'channel' : 'my_channel', 'message' : event })
+        info = pubnub.publish().channel('my_channel').message({'text': event}).pn_async(my_publish_callback)
+        print(info)
+        
+
+    # def sent_event(self):
+    #     id = self.verify_id(self.id)
+    #     event = f'New Student Added - {id}'
+    #     pub = pubnub.publish().channel("my_channel")
+    #     msg = pub.message(str(event)).pn_async(my_publish_callback)
+
+    #     print("msg: " + msg)
     
 
