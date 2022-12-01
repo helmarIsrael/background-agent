48f1411254a04c87a98172bd7b2da61348124409
mjomandam07 <mjomandam@gmail.com>
2022-12-01 16:11:49 +0800

    Currently working on parents post action and is currently analyzing why the stored procedure returns none as response.

--- postgresql-files/sql2.sql
+++ postgresql-files/sql2.sql
@@ -1,7 +1,15 @@
-SELECT updatepassword('john.doe', '1');
+SELECT updatepassword('paeng.faker', '1');
 
--- SELECT getpersonidbyusername('father.doe');
+-- SELECT getpersonidbyusername('markpapi.faker');
 
--- select * from getmykids('P2022CARDOMAGTANGGOL-23', 'students');
+-- select * from getmykids('P2022CARDOMAGTANGGOL-27', 'students');
 
-select * from personlevel WHERE studentid ='S2022CARDOMAGTANGGOL-13' and semid = '20222023';
+-- select * from personlevel WHERE studentid ='S2022CARDOMAGTANGGOL-13' and semid = '20222023';
+
+
+-- select * from person where personid IN
+--                       (
+--                         select personid from getclasslist(
+--                         'AERO', 5, 'XXX', '20222023')
+                        
+--                         ) 
--- postgresql-files/sql4.sql
+++ postgresql-files/sql4.sql
@@ -1,19 +1,19 @@
 -- insert into personschool (personnumid, schoolid, iscurrent, status_, semid) 
--- values ('S2022CARDOMAGTANGGOL-13', 'XXX', true, 'NEW', getcurrsem());
+-- values ('S2022CARDOMAGTANGGOL-14', 'XXX', true, 'NEW', getcurrsem());
 
-select insup2personlevel(
- 'elem',
- 5,
- 'S2022CARDOMAGTANGGOL-13',
- 'F2018CARDOMAGTANGGOL-1',
- getcurrsem(),
- 'XXX',
- 'AERO');
+-- select insup2personlevel(
+--  'elem',
+--  5,
+--  'S2022CARDOMAGTANGGOL-14',
+--  'F2018CARDOMAGTANGGOL-1',
+--  getcurrsem(),
+--  'XXX',
+--  'AERO');
  
  
-select enrollstudent(
-	'S2022CARDOMAGTANGGOL-13',
-	'XXX',
-	getcurrsem(),
-	5,
-	'AERO');
+-- select enrollstudent(
+-- 	'S2022CARDOMAGTANGGOL-14',
+-- 	'XXX',
+-- 	getcurrsem(),
+-- 	5,
+-- 	'AERO');
