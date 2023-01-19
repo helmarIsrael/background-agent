b41acc963690977b4fe517923d35342da5216567
mjomandam07 <mjomandam@gmail.com>
2023-01-19 11:35:45 +0800

    Changed the sms sending package to url_launcher. The messages are now being send however there is a problem in sending sms with multiple recipients and sending without opeining the messages app.

--- flutter/sms_server/lib/utils/sms_sender.dart
+++ flutter/sms_server/lib/utils/sms_sender.dart
@@ -1,10 +1,28 @@
 import 'package:flutter_sms/flutter_sms.dart';
 
 void send_sms(String message, List<String> recipents) async {
-  String _result =
-      await sendSMS(message: message, recipients: recipents, sendDirect: false);
+  var _result = sendSMS(message: message, recipients: recipents);
   bool canSend = await canSendSMS();
 
   print('Device Status: $canSend');
   print(_result);
 }
+
+// import 'dart:io';
+
+// import 'package:url_launcher/url_launcher.dart';
+
+// void send_sms(String message, List recipients) async {
+//   // String get; separator => isCupertino() ? '&' : '?';
+//   try {
+//     if (Platform.isAndroid) {
+//       String numbers = recipients.join(';');
+//       print(numbers);
+//       var url = 'sms:/open?addresses=${recipients}?body=$message';
+//       var status = await launch(url);
+//       print(status);
+//     }
+//   } catch (e) {
+//     print(e);
+//   }
+// }
